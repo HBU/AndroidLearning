33f94aac1ba88533308bc893015eedea21c062dd
David <8584751@qq.com>
2017-12-26 02:56:34 +0800

    DrawLayer

--- CalculatorDrawerLayout/app/src/main/java/com/example/calculatorui/MainActivity.java
+++ CalculatorDrawerLayout/app/src/main/java/com/example/calculatorui/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculatorui;
 import android.content.DialogInterface;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
@@ -22,102 +24,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //toolbar.setTitle("Title");
-        //toolbar.setSubtitle("SubTitle");
-        toolbar.setLogo(R.mipmap.logo);
-        //toolbar.setNavigationIcon(R.mipmap.ic_launcher);
-
-        //设置导航图标、添加菜单点击事件要在setSupportActionBar方法之后
-        setSupportActionBar(toolbar);
-
-
-
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()) {
-                    case  R.id.action_search:
-                        new AlertDialog.Builder(MainActivity.this)
-                                .setIcon(R.mipmap.logo)
-                                .setTitle("计算器")
-                                .setMessage("上传到服务器?")
-                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                        Toast.makeText(MainActivity.this,"已上传到服务器",Toast.LENGTH_SHORT).show();
-                                    }
-                                })
-                                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                        Toast.makeText(MainActivity.this,"继续使用计算器",Toast.LENGTH_SHORT).show();
-                                    }
-                                }).create().show();
-                        break;
-                    case  R.id.action_notifications:
-                        new AlertDialog.Builder(MainActivity.this)
-                                .setIcon(R.mipmap.logo)
-                                .setTitle("计算器")
-                                .setMessage("配置计算器?")
-                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                        Toast.makeText(MainActivity.this,"配置完毕",Toast.LENGTH_SHORT).show();
-                                    }
-                                })
-                                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                        Toast.makeText(MainActivity.this,"继续使用计算器",Toast.LENGTH_SHORT).show();
-                                    }
-                                }).create().show();
-                        break;
-                    case  R.id.action_settings:
-                        new AlertDialog.Builder(MainActivity.this)
-                                .setIcon(R.mipmap.logo)
-                                .setTitle("计算器")
-                                .setMessage("确定要退出么?")
-                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                        finish();//Exit Activity
-                                    }
-                                })
-                                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                        Toast.makeText(MainActivity.this,"继续使用计算器",Toast.LENGTH_SHORT).show();
-                                    }
-                                }).create().show();
-                        break;
-                    case R.id.action_settings1:
-                        new AlertDialog.Builder(MainActivity.this)
-                                .setIcon(R.mipmap.logo)
-                                .setTitle("计算器")
-                                .setMessage("计算器 1.0 \n河北大学 \n网络空间安全与计算机学院 \nDavid \n2017.12.24")
-                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                    }
-                                })
-                                .create().show();
-                        break;
-
-
-                }
-                return true;
-            }
-        });
-    }
-    @Override
-    public void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        //Toolbar 必须在onCreate()之后设置标题文本，否则默认标签将覆盖我们的设置
-        if (toolbar != null) {//mActionBarToolbar就是android.support.v7.widget.Toolbar
-            toolbar.setTitle("");//设置为空，可以自己定义一个居中的控件，当做标题控件使用
+//        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
+//        navView.setCheckedItem(R.id.nav_call);
+//        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(MenuItem item) {
+//                mDrawerLayout.closeDrawers();
+//                return true;
+//            }
+//        });
     }
-    @Override
+
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.toolbar, menu);
         return true;
     }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                mDrawerLayout.openDrawer(GravityCompat.START);
+//                break;
+//            case R.id.backup:
+//                Toast.makeText(this, "You clicked Backup", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.delete:
+//                Toast.makeText(this, "You clicked Delete", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.settings:
+//                Toast.makeText(this, "You clicked Settings", Toast.LENGTH_SHORT).show();
+//                break;
+//            default:
+//        }
+//        return true;
+//    }
+
 }
---
+++ DrawLayoutTest/app/src/main/java/com/example/drawlayouttest/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.drawlayouttest;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+//    private  Toolbar toolbar;
+    private DrawerLayout mDrawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);//这句代码加上会引起app崩掉，原因在于没有修改styles。。。20171226
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+//            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.backup:
+                Toast.makeText(this, "You clicked Backup", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this, "You clicked Delete", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this, "You clicked Settings", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+}
