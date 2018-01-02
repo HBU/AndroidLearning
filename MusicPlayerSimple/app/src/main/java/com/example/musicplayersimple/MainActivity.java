eb4128555eb67b6066fa0aab07bd886807bcd44b
David <8584751@qq.com>
2018-01-02 23:47:01 +0800

    增加 暂停 功能
    听同一首歌进行测试，听到恶心 (⊙o⊙)…

--- MusicPlayerSimple/app/src/main/java/com/example/musicplayersimple/MainActivity.java
+++ MusicPlayerSimple/app/src/main/java/com/example/musicplayersimple/MainActivity.java
@@ -7,32 +7,38 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    int Flag = 1;
+    Button playButton;
+    Button stopButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();// 设置按钮和监听
-    }
-
-    private void initView(){
-        Button playButton = ( Button )super.findViewById( R.id.play );
-        Button stopButton = ( Button )super.findViewById( R.id.stop );
+        playButton = ( Button )findViewById( R.id.play );
+        stopButton = ( Button )findViewById( R.id.stop );
         playButton.setOnClickListener( clickListener );
         stopButton.setOnClickListener( clickListener );
     }
 
-    private View.OnClickListener clickListener = new View.OnClickListener() {
-
+     private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent intent = new Intent(MainActivity.this,MyMusicService.class );//连接服务
             switch ( v.getId() ) {
                 case R.id.play:
                     startService(intent);//开启服务
+                    if (Flag == 1) {
+                        playButton.setText("暂停");
+                        Flag = 0;
+                    }else {
+                        playButton.setText("继续播放");
+                        Flag = 1;
+                    }
                     break;
                 case R.id.stop:
                     stopService(intent);//关闭服务
+                    playButton.setText("播放");
+                    Flag = 1;
                     break;
                 default:
                     break;
--- MusicPlayerSimple/app/src/main/java/com/example/musicplayersimple/MyMusicService.java
+++ MusicPlayerSimple/app/src/main/java/com/example/musicplayersimple/MyMusicService.java
@@ -8,6 +8,7 @@ import android.os.IBinder;
 public class MyMusicService extends Service {
     public MyMusicService() {
     }
+
     private MediaPlayer mediaPlayer;
 
     @Override
@@ -19,13 +20,27 @@ public class MyMusicService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        this.mediaPlayer = MediaPlayer.create( this, R.raw.despacio);//音乐文件资源路径
-        this.mediaPlayer.start();
+
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        this.mediaPlayer.stop();
+        mediaPlayer.stop();
     }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand( intent,  flags,  startId);
+        if (mediaPlayer!=null && mediaPlayer.isPlaying()){//如果正在播放
+            mediaPlayer.pause();
+        } else if (mediaPlayer!=null){//如果处于暂停状态
+                mediaPlayer.start();
+            }else{//如果尚未开始播放
+                mediaPlayer = MediaPlayer.create(this, R.raw.despacio);//音乐文件资源路径
+                mediaPlayer.start();
+            }
+        return START_NOT_STICKY;
+    }
+
 }
