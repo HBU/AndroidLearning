5d12986492ebb8a2f3f36908632326a3da6cbfe4
David <8584751@qq.com>
2018-01-31 01:32:26 +0800

    每周统计

--- TimeStatistic/app/src/main/java/cn/hbu/cs/timestatistic/MainActivity.java
+++ TimeStatistic/app/src/main/java/cn/hbu/cs/timestatistic/MainActivity.java
@@ -1,32 +1,115 @@
 package cn.hbu.cs.timestatistic;
 
+import android.Manifest;
 import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+//Ref:https://github.com/Uyouii/Statistics
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button button;
+    private int style;
+    private long totalTime;
+    private int totalTimes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textView =(TextView)findViewById(R.id.title);
+        textView.setText("请设置权限！");
+        this.style = StatisticsInfo.WEEK;//DAY is NULL ,Why ?
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    if(!isStatAccessPermissionSet(MainActivity.this)) {
+                        startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));   //查看是否为应用设置了权限
+                    }
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
         try {
-            if (!isStatAccessPermissionSet(MainActivity.this)) {
-//                startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));   //查看是否为应用设置了权限
-                Toast.makeText(getApplicationContext(), "Please Open: Get info about installed apps", Toast.LENGTH_SHORT).show();    //显示toast信息
-            } else {
-                Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_SHORT).show();
+            if((isStatAccessPermissionSet(this))){
+                textView.setVisibility(View.INVISIBLE);
+                button.setVisibility(View.INVISIBLE);
             }
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
+        List<Map<String,Object>> datalist = null;
+        StatisticsInfo statisticsInfo = new StatisticsInfo(this,this.style);
+        totalTime = statisticsInfo.getTotalTime();
+        totalTimes = statisticsInfo.getTotalTimes();
+        datalist = getDataList(statisticsInfo.getShowList());
+
+        ListView listView = (ListView)findViewById(R.id.AppStatisticsList);
+        SimpleAdapter adapter = new SimpleAdapter(this,datalist,R.layout.inner_list,
+                new String[]{"label","info","times","icon"},
+                new int[]{R.id.label,R.id.info,R.id.times,R.id.icon});
+        listView.setAdapter(adapter);
+
+        adapter.setViewBinder(new SimpleAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Object o, String s) {
+                if(view instanceof ImageView && o instanceof Drawable){
+                    ImageView iv=(ImageView)view;
+                    iv.setImageDrawable((Drawable)o);
+                    return true;
+                }
+                else return false;
+            }
+        });
+    }
+    private List<Map<String,Object>> getDataList(ArrayList<AppInformation> ShowList) {
+        List<Map<String, Object>> dataList = new ArrayList<Map<String, Object>>();
+
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("label","全部应用");
+        map.put("info","运行时间: " + DateUtils.formatElapsedTime(totalTime / 1000));
+        map.put("times","本次开机操作次数: " + totalTimes);
+        map.put("icon",R.drawable.use);
+        dataList.add(map);
+
+        for(AppInformation appInformation : ShowList) {
+            map = new HashMap<String,Object>();
+            map.put("label",appInformation.getLabel());
+            map.put("info","运行时间: " + DateUtils.formatElapsedTime(appInformation.getUsedTimebyDay() / 1000));
+            map.put("times","本次开机操作次数: " + appInformation.getTimes());
+            map.put("icon",appInformation.getIcon());
+            dataList.add(map);
+        }
+
+        return dataList;
     }
     private boolean isStatAccessPermissionSet(Context c) throws PackageManager.NameNotFoundException {
         PackageManager packageManager = c.getPackageManager();
